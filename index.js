2c9b1dff8a60cfd3ae8b98b90176a6f112279130
ObjectiveTruth <miguel@objectivetruth.ca>
2022-10-31 18:50:20 -0400

    Added a reference to the config file being 2 directories up
    This means it can be used as a node npm package since it will find the config in the root directory of the package requiring it

---
+++ index.js
@@ -0,0 +1,9 @@
+var Application = require("./lib/app");
+var Server = require("./lib/server");
+var sdk = require("./lib/sdk");
+
+module.exports = {
+    Application: Application,
+    Server: Server,
+    sdk: sdk
+};
